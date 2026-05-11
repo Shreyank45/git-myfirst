420b1b1db6abe2c3d91ffc77464273dae1460147
Shreyank <shreyankkj7022@gmail.com>
2026-05-11 19:48:30 +0530

    My code commit

---
+++ nthPrime.java
@@ -0,0 +1,29 @@
+package com.kjs.Prime;
+import java.util.*;
+public class nPrimeNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the value");
+		int n  = sc.nextInt();
+		ifprime(n);
+		}
+	static void ifprime(int n)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			if(isprime(i))
+				System.out.println(i);
+		}
+	}
+	static boolean isprime (int c)
+	{
+		for(int i=2;i*i<=c;i++)
+		{
+			if(c%i==0)
+				return false;
+			
+		}
+		return true;
+	}
+
+}
